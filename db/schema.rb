25e0b9aa2e96f19f91dc266a3008b4d5ea0de3cd
Bradley Showen <bshowen@me.com>
2020-05-23 16:20:15 -0400

    create devise users

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_05_10_235137) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"

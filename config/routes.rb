5682513af6c9a4ebf9b7a5d2b6e9a390c26fe364
Bradley Showen <bshowen@me.com>
2020-06-06 07:17:06 -0400

    log in, log out, sign up working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get 'site/index'
+  devise_for :users 
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'site#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
